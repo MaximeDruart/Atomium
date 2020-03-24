18b96de0736f65d30ea7a7936cc996bebdbe623d
ZoeLsf <lesouef.zoe@gmail.com>
2020-03-24 23:31:04 +0100

    Create timeline

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react"
 import logo from "./../../Images/logo.png"
 
+
 const Header = () => {
-  const [hovering, setHovering] = useState(false)
-  const onMouseEnterHandler = event => setHovering(true)
-  const onMouseLeaveHandler = event => setHovering(false)
+
+  // Hover header hook 
+  const [hovering1, setHovering1] = useState(false)
+  const onMouseEnterHandler1 = event => setHovering1(true)
+  const onMouseLeaveHandler1 = event => setHovering1(false)
+
+  const [hovering2, setHovering2] = useState(false)
+  const onMouseEnterHandler2 = event => setHovering2(true)
+  const onMouseLeaveHandler2 = event => setHovering2(false)
+
+  const [hovering3, setHovering3] = useState(false)
+  const onMouseEnterHandler3 = event => setHovering3(true)
+  const onMouseLeaveHandler3 = event => setHovering3(false)
+
+  const [hovering4, setHovering4] = useState(false)
+  const onMouseEnterHandler4 = event => setHovering4(true)
+  const onMouseLeaveHandler4 = event => setHovering4(false)
+
 
   // condition ? si vrai : si faux
   return (
@@ -12,19 +28,31 @@ const Header = () => {
       <img src={logo} alt="logo" className="header_logo"></img>
       <div className="header_links">
         <a
-          onMouseEnter={onMouseEnterHandler}
-          onMouseLeave={onMouseLeaveHandler}
-          className={hovering ? "header_link_1 header_link_hovered" : "header_link"}
+          onMouseEnter={onMouseEnterHandler1}
+          onMouseLeave={onMouseLeaveHandler1}
+          className={hovering1 ? "header_link header_link_hovered" : "header_link"}
           href="#">
           01
         </a>
-        <a className="header_link_2" href="">
+        <a 
+          onMouseEnter={onMouseEnterHandler2}
+          onMouseLeave={onMouseLeaveHandler2}
+          className={hovering2 ? "header_link header_link_hovered" : "header_link"}
+          href="#">
           02
         </a>
-        <a className="header_link_3" href="">
+        <a 
+          onMouseEnter={onMouseEnterHandler3}
+          onMouseLeave={onMouseLeaveHandler3}
+          className={hovering3 ? "header_link header_link_hovered" : "header_link"}
+          href="#">
           03
         </a>
-        <a className="header_link_4" href="">
+        <a 
+          onMouseEnter={onMouseEnterHandler4}
+          onMouseLeave={onMouseLeaveHandler4}
+          className={hovering4 ? "header_link header_link_hovered" : "header_link"}
+          href="#">
           04
         </a>
       </div>
--- src/Components/UI/UserInterface.js
+++ src/Components/UI/UserInterface.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./Header"
+import Timeline from "./Timeline"
 import "./UI.scss"
 
 
@@ -7,6 +8,9 @@ const UserInterface = () => {
   return (
     <div className="ui">
       <Header />
+      <div className="main_container">
+        <Timeline />
+      </div> 
     </div>
   )
 }
