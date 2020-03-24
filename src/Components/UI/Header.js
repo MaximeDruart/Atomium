c498f816293bd636d85dbef2f75e1166fc61a6d6
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-24 18:58:51 +0100

    header hover animation

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,19 +1,34 @@
-import React from "react"
-import logo from './../../Images/logo.png'
+import React, { useState } from "react"
+import logo from "./../../Images/logo.png"
 
-const Header = () => 
-{
-  return(
-      <div className="header">
-        <img src={logo} alt="logo" className="header_logo"></img>
-        <div className="header_links">
-          <a className="header_link_1" href="">01</a>
-          <a className="header_link_2" href="">02</a>
-          <a className="header_link_3" href="">03</a>
-          <a className="header_link_4" href="">04</a>
-        </div>
+const Header = () => {
+  const [hovering, setHovering] = useState(false)
+  const onMouseEnterHandler = event => setHovering(true)
+  const onMouseLeaveHandler = event => setHovering(false)
+
+  // condition ? si vrai : si faux
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" className="header_logo"></img>
+      <div className="header_links">
+        <a
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
+          className={hovering ? "header_link_1 header_link_hovered" : "header_link"}
+          href="#">
+          01
+        </a>
+        <a className="header_link_2" href="">
+          02
+        </a>
+        <a className="header_link_3" href="">
+          03
+        </a>
+        <a className="header_link_4" href="">
+          04
+        </a>
       </div>
-      
+    </div>
   )
 }
 
