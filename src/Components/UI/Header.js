ff6faaf76a85afa8fa4d6f3fd1942708df985abf
ZoeLsf <lesouef.zoe@gmail.com>
2020-03-24 18:44:18 +0100

    Create Header

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,7 +1,20 @@
 import React from "react"
+import logo from './../../Images/logo.png'
 
-const Header = () => {
-  return <header>header</header>
+const Header = () => 
+{
+  return(
+      <div className="header">
+        <img src={logo} alt="logo" className="header_logo"></img>
+        <div className="header_links">
+          <a className="header_link_1" href="">01</a>
+          <a className="header_link_2" href="">02</a>
+          <a className="header_link_3" href="">03</a>
+          <a className="header_link_4" href="">04</a>
+        </div>
+      </div>
+      
+  )
 }
 
 export default Header
--- src/Components/UI/UserInterface.js
+++ src/Components/UI/UserInterface.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./Header"
 import "./UI.scss"
 
+
 const UserInterface = () => {
   return (
     <div className="ui">
