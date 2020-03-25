8706341a43edb4c0a0b32e68282b70eb17a02e01
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-25 14:55:04 +0100

    updated data structure

--- src/components/UI/Scene2.js
+++ src/components/UI/Scene2.js
@@ -1,10 +1,19 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import "./Scene2.scss"
 import scene2data from "./Scene2data"
+import Timeline from "./Timeline"
 
 const Scene2 = () => {
   const { updateContext, activeScene, activeScene2Atom } = useContext()
-  return <div className="scene-2"></div>
+  const [activeDescription, setActiveDescription] = useState(0)
+  return (
+    <div className="scene-2">
+      <div className="title">Les 3 grandes molécules</div>
+      <Timeline />
+      <div className="molecule-name">{scene2data[activeScene2Atom].name}</div>
+      <div className="description">{scene2data[activeScene2Atom].description[activeDescription]}</div>
+    </div>
+  )
 }
 
 export default Scene2
--- src/components/UI/Scene2data.js
+++ src/components/UI/Scene2data.js
@@ -1,20 +1,14 @@
 export default [
   {
     name: "carbone",
-    p1: "",
-    p2: "",
-    p3: ""
+    descriptions: ["desc1", "desc2", "desc3"]
   },
   {
     name: "hydrogene",
-    p1: "",
-    p2: "",
-    p3: ""
+    descriptions: ["desc1", "desc2", "desc3"]
   },
   {
     name: "oxygene",
-    p1: "",
-    p2: "",
-    p3: ""
+    descriptions: ["desc1", "desc2", "desc3"]
   }
 ]
