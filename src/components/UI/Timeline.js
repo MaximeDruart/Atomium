53d716dc9fbf488d9f7e8f8092cc1846924fad47
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-25 11:28:59 +0100

    timeline click handling

--- src/components/UI/Timeline.js
+++ src/components/UI/Timeline.js
@@ -1,45 +1,22 @@
 import React, { useState } from "react"
 
 const Timeline = () => {
-    const [click1, setClick1] = useState(false)
-    const onMouseClickHandler1 = event => setClick1(true)
+  const [activePoint, setActivePoint] = useState(0)
 
-    const [click2, setClick2] = useState(false)
-    const onMouseClickHandler2 = event => setClick2(true)
+  const onMouseClickHandler = index => {
+    setActivePoint(index)
+  }
 
-    const [click3, setClick3] = useState(false)
-    const onMouseClickHandler3 = event => setClick3(true)
-
-    // if (click1)
-    // {
-    //     const onMouseClickHandler2 = event => setClick2(false)
-    //     const onMouseClickHandler3 = event => setClick3(false)
-
-    // }else if (click2)
-    // {
-    //     const onMouseClickHandler1 = event => setClick1(false)
-    //     const onMouseClickHandler3 = event => setClick3(false)
-    // }else if (click3)
-    // {
-    //     const onMouseClickHandler1 = event => setClick1(false)
-    //     const onMouseClickHandler2 = event => setClick2(false)
-    // }
-
-    const PointClicked = () =>
-    {
-
-    }
-
-    return(
-        <div className="timeline">
-            <div className="timeline_line"></div>
-            <div className="timeline_points">
-                <div onClick={onMouseClickHandler1} className={click1 ? "point_selected" : "point"}></div>
-                <div onClick={onMouseClickHandler2} className={click2 ? "point_selected" : "point"}></div>
-                <div onClick={onMouseClickHandler3} className={click3 ? "point_selected" : "point"}></div>
-            </div> 
-        </div>
-    )
+  return (
+    <div className="timeline">
+      <div className="timeline_line"></div>
+      <div className="timeline_points">
+        <div onClick={() => onMouseClickHandler(0)} className={activePoint === 0 ? "point_selected" : "point"}></div>
+        <div onClick={() => onMouseClickHandler(1)} className={activePoint === 1 ? "point_selected" : "point"}></div>
+        <div onClick={() => onMouseClickHandler(2)} className={activePoint === 2 ? "point_selected" : "point"}></div>
+      </div>
+    </div>
+  )
 }
 
 export default Timeline
